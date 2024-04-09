9fdf5119736383d18895a6edc80f27199562f0b4
MahdiArabpour <mahdi.a.200066@gmail.com>
2024-04-09 10:42:21 +0330

    fixed some bug

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "f33b42475e5befec0e6d955f6d806f33",
-"index.html": "508a2a40b98b3c2cb8f8c6016667148d",
-"/": "508a2a40b98b3c2cb8f8c6016667148d",
+"index.html": "47161ca32c7106eab95926f2b8536c29",
+"/": "47161ca32c7106eab95926f2b8536c29",
 "CNAME": "ddd29ef463015c2cb2fafc1069a6f349",
-"main.dart.js": "1a4c01714942b91c08b71278eaacf41d",
+"main.dart.js": "56c12eba275fbe9e2cf91d2b8af16903",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "0080481723f567771144f9b1f21e0927",
 "icons/Icon-192.png": "59339a702e78d4c303e9fda7854fc92e",
